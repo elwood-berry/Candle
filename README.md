597cae7ccb61988a346f18e3dfe1cce965da350f
elwood-berry <eberry@anterasoftware.com>
2021-09-07 23:00:21 -0500

    7 files modified, 5 added, 1 deleted

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Candle
 
 Simulate the story of a japanese candle through CSS animation.
+
+---  
+  
+## References  
+
+1. [Roboto - Designed by Christian Robertson](https://fonts.google.com/specimen/Roboto)  
+1. [@angular/flex-layout](https://www.npmjs.com/package/@angular/flex-layout)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, FlexLayoutModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
